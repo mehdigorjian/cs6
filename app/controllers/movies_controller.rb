35527668afeba66edda94597efef42485b4d01b9
mehdigorjian <mehdi.gorjian@gmail.com>
2021-02-15 20:19:40 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,25 +1,31 @@
 class MoviesController < ApplicationController
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
+
   def index
-    @movies = Movie.all_ratings
     @all_ratings = Movie.all_ratings
     if params[:ratings]
       @ratings_to_show = params[:ratings].keys
+      session[:ratings] = params[:ratings]
+    elsif session[:ratings]
+      @ratings_to_show = session[:ratings].keys
     else
-      @ratings_to_show = []
+      @ratings_to_show = @all_ratings
     end
     
     @ratings_to_show.each do |rating|
       params[rating] = true
     end
-
-    @movies = Movie.with_ratings(@ratings_to_show)
+    
     if params[:sort]
       @movies = Movie.with_ratings(@ratings_to_show).order(params[:sort])
+      session[:sort] = params[:sort]
+    elsif session[:sort]
+      @movies = Movie.with_ratings(@ratings_to_show).order(session[:sort])
     else
       @movies = Movie.with_ratings(@ratings_to_show)
     end
@@ -28,18 +34,35 @@ class MoviesController < ApplicationController
   def new
     # default: render 'new' template
   end
+
   def create
     @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
+
   def edit
     @movie = Movie.find params[:id]
   end
+
   def update
     @movie = Movie.find params[:id]
     @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+
+  private
+  # Making "internal" methods private is not required, but is a common practice.
+  # This helps make clear which methods respond to requests, and which ones do not.
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
 end
